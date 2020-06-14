d2c5bce7ecdcdcd33a0b90701152d1f50b77b47a
modfort <mehdi-098@outlook.fr>
2020-06-15 01:50:55 +0200

    Ajoute des nouvelles pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import "./app.css";
 import { NavLink as Link, Switch, Route } from "react-router-dom";
 import TogoList from "./pages/art/PaintingData";
 import Instructions from "./pages/instructions";
-
+import Myexample from "./pages/example/exemple";
+import Exercice3 from "./pages/exercice3/exemple3";
+import Exercice4 from "./pages/exercice4/exemple4";
 function App() {
   return (
     <div className="App">
       <header>
-        <Link to="/">Home</Link>
-        <Link to="/instructions">Instructions</Link>
-        <Link to="/art/example">Example</Link>
+
+        <div className="navbar">
+          <div><Link to="/">Home    </Link></div>
+          <div><Link to="/instructions">Instructions  </Link></div>
+          <div><Link to="/first">Example  </Link></div>
+          <div><Link to="/example">Myexample  </Link></div>
+          <div><Link to="/exercice3">Exercice3  </Link></div>
+
+        </div>
       </header>
       <main>
         <Switch>
@@ -22,17 +30,33 @@ function App() {
                 </span>
                 The MET challenge!
               </h1>
+
               <p>Click on "Instructions" to procede. </p>
               <p>Good luck :)</p>
+
+             
             </div>
           </Route>
+
           <Route exact path="/instructions">
             <Instructions />
           </Route>
-          <Route exact path="/art/example">
+
+          <Route exact path="/first/">
             <TogoList />
           </Route>
-          <Route path="/art/:id"></Route>
+
+          <Route exact path="/example">
+            <Myexample />
+          </Route>
+
+          <Route exact path="/exercice3">
+            <Exercice3 />
+          </Route>
+
+          <Route path="/art/:id" children={<Exercice4/>}/>
+            
+
           <Route path="/">404 : Nothing here...</Route>
         </Switch>
       </main>
